f1f5b286c2502bfdf9b21489f7b3bd578b0da6fd
hoangtin04 <hoangtin21082004@gmail.com>
2024-10-19 10:03:33 +0700

    Phiếu xuất + style

--- frontend/src/pages/ImportForm.jsx
+++ frontend/src/pages/ImportForm.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../style/ImportForm.css'
+const NhapHang=()=>{
+    return (
+        <div>
+            
+        </div>
+    );
+};
 const ImportForm=()=>{
     return (
         <div>
-            <h1>trang EntryForm</h1>
+            <div className='title-Selection'>
+                <p className='titleNH'>Nhập hàng</p>
+                <p className='titlePN'>Phiếu Nhập</p>
+            </div>
+            <NhapHang/>
         </div>
     );
 };
