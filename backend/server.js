3ec96b0f70f2fd31f0ebc73c8d3abd0cc00af9e8
laithethanh <laithanh@1006@gmail.com>
2024-10-25 16:51:03 +0700

    cmt

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3001, () => {
+    console.log(`Server is running on port 3001`);
 });
--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 const Product = {
     getAll: () => {
         return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM sanpham', (err, results) => {
+            db.query('SELECT * FROM san_pham', (err, results) => {
                 if (err) reject(err);
                 resolve(results);
             });
@@ -11,7 +11,7 @@ const Product = {
     },
     getById: (id) => {
         return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM sanpham WHERE masp = ?', [id], (err, result) => {
+            db.query('SELECT * FROM san_pham WHERE masp = ?', [id], (err, result) => {
                 if (err) reject(err);
                 resolve(result[0]);
             });
@@ -19,7 +19,7 @@ const Product = {
     },
     create: (productData) => {
         return new Promise((resolve, reject) => {
-            db.query('INSERT INTO sanpham SET ?', productData, (err, result) => {
+            db.query('INSERT INTO san_pham SET ?', productData, (err, result) => {
                 if (err) reject(err);
                 resolve({ id: result.insertId, ...productData });
             });
@@ -27,7 +27,7 @@ const Product = {
     },
     update: (id, productData) => {
         return new Promise((resolve, reject) => {
-            db.query('UPDATE sanpham SET ? WHERE masp = ?', [productData, id], (err, result) => {
+            db.query('UPDATE san_pham SET ? WHERE masp = ?', [productData, id], (err, result) => {
                 if (err) reject(err);
                 resolve(result);
             });
@@ -35,7 +35,7 @@ const Product = {
     },
     delete: (id) => {
         return new Promise((resolve, reject) => {
-            db.query('DELETE FROM sanpham WHERE masp = ?', [id], (err, result) => {
+            db.query('DELETE FROM san_pham WHERE masp = ?', [id], (err, result) => {
                 if (err) reject(err);
                 resolve(result);
             });
