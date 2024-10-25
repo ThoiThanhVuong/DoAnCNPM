08115b966b697ccdaeabe21d23b2342f4729350b
laithethanh <laithanh@1006@gmail.com>
2024-10-25 15:07:12 +0700

    cmt

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database:'quanlidienthoai'
+    database:'quanlikhohang'
 });
 
 // Kết nối tới cơ sở dữ liệu
