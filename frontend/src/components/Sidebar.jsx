ebf95b391b7afad79d710d583e49d02aae618de3
laithethanh <laithanh@1006@gmail.com>
2024-11-06 21:36:56 +0700

    cmt

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -15,17 +15,10 @@ import {
   FaUserShield,
 } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import SignIn from "./SignIn/SignIn";
 
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-  const [formSignin, setFormSignin] = useState(false);
-  const handleShowForm = () => {
-    setFormSignin(!formSignin);
-  };
-  const [login, setLogin] = useState(true);
-  const [menuShow, setMenuShow] = useState([]);
   const menuItem = [
     {
       path: "/homePage",
@@ -88,7 +81,7 @@ const Sidebar = ({ children }) => {
       icon: <FaChartBar />,
     },
   ];
-  return login ? (
+  return (
     <div className="container">
       <div style={{ width: isOpen ? "250px" : "50px" }} className="sidebar">
         <div className="top_section">
@@ -122,50 +115,6 @@ const Sidebar = ({ children }) => {
         ))}
       </div>
       <main>{children}</main>
-    </div>
-  ) : (
-    <div className="container">
-      <div style={{ width: isOpen ? "250px" : "50px" }} className="sidebar">
-        <div className="top_section">
-          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
-            Logo
-          </h1>
-          <div style={{ marginLeft: isOpen ? "90px" : "0px" }} className="bars">
-            <FaBars onClick={toggle} />
-          </div>
-        </div>
-        {menuShow.map((item, index) => (
-          <NavLink
-            to={item.path}
-            key={index}
-            className="link"
-            activeClassName="active"
-          >
-            <div
-              style={{ paddingLeft: isOpen ? "5px" : "10px" }}
-              className="icon"
-            >
-              {item.icon}
-            </div>
-            <div
-              style={{ display: isOpen ? "block" : "none" }}
-              className="link_text"
-            >
-              {item.name}
-            </div>
-          </NavLink>
-        ))}
-      </div>
-      <main className="signin">
-        {!formSignin ? (
-          <div className="signin-open-btn">
-            <h1>Ban Chua Dang Nhap</h1>
-            <button onClick={handleShowForm}>Click to Sign In</button>
-          </div>
-        ) : (
-          <SignIn />
-        )}
-      </main>
     </div>
   );
 };
