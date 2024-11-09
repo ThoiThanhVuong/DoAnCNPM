6ba0a9994dd54a9c09e7decf4bb6913eda588e39
phu3390 <you@example.com>
2024-11-09 18:53:20 +0700

    model provider

---
+++ backend/src/models/ProviderModel.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const ProviderModel = sequelize.define('Provider', {
+    ma_ncc: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    ten_ncc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    dia_chi: {
+        type: DataTypes.STRING,
+    },
+    email_ncc: {
+        type: DataTypes.STRING,
+    },
+    sdt_ncc: {
+        type: DataTypes.STRING,
+    },
+}, { tableName: 'nha_cung_cap', timestamps: false });
