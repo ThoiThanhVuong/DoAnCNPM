1dcc876074250bb2f3653ed3bc618e2c9aa275dc
phu3390 <you@example.com>
2024-11-10 20:24:06 +0700

    update customerControllers

--- backend/src/controllers/customerController.js
+++ backend/src/controllers/customerController.js
@@ -26,41 +26,52 @@ const getCustomerByMaKH = async (req, res) => {
 
 // Add a new Customer
 const addCustomer = async (req, res) => {
-  // Có thể kiểm tra dữ liệu đầu vào ở đây
-  try {
-    const savedCustomer = await Customer.create(req.body);
-    res.status(201).json(savedCustomer);
-  } catch (error) {
-    res.status(500).json({ message: 'Lỗi khi thêm khách hàng', error: error.message });
-  }
+  const { id,name, adress, phone_number } = req.body;
+    try {
+        const newCustomer = await Customer.create({
+            id,
+            name,
+            adress,
+            phone_number
+        });
+        res.status(201).json(newCustomer);
+        res.json({ message: 'Khách hàng đã được thêm' });
+    } catch (error) {
+        res.status(500).json({ error: 'Lỗi khi thêm khách hàng' });
+    }
 };
 
 // Update an Customer by ma_kh
 const updateCustomer = async (req, res) => {
-  const { ma_nv } = req.params;
+  const { ma_kh } = req.params;
+  const { name, adress, phone_number } = req.body;
   try {
-    const updatedCustomer = await Customer.update(ma_kh, req.body);
-    if (updatedCustomer[0] === 0) {  // Kiểm tra affectedRows
-      return res.status(404).json({ message: 'Không tìm thấy khách hàng để cập nhật' });
+    const updatedCustomer = await Customer.findByPk(ma_kh);
+    if (!updatedCustomer) return res.status(404).json({ error: 'Không tìm thấy khách hàng' });
+        
+    updatedCustomer.name = name;
+    updatedCustomer.adress = adress;
+    updatedCustomer.phone_number = phone_number;
+        await updatedCustomer.save();
+
+        return res.json(updatedCustomer);
+    } catch (error) {
+        res.status(500).json({ error: 'Lỗi khi cập nhật khách hàng' });
     }
-    res.status(200).json({ message: 'Khách hàng cập nhật thành công' });
-  } catch (error) {
-    res.status(500).json({ message: 'Cập nhật khách hàng không thành công', error: error.message });
-  }
 };
 
 // Delete an Customer by ma_kh
 const deleteCustomer = async (req, res) => {
   const { ma_kh } = req.params;
   try {
-    const deletedCustomer = await Customer.delete(ma_kh);
-    if (deletedCustomer === 0) {  // Kiểm tra affectedRows
-      return res.status(404).json({ message: 'Không tìm thấy khách hàng để xóa' });
+    const deletedCustomer = await Customer.findByPk(ma_kh);
+    if (!deletedCustomer) return res.status(404).json({ error: 'Không tìm thấy khách hàng' });
+        
+        await deletedCustomer.destroy();
+        res.json({ message: 'Khách hàng đã được xóa' });
+    } catch (error) {
+        res.status(500).json({ error: 'Lỗi không xóa được' });
     }
-    res.status(200).json({ message: 'Xóa khách hàng thành công' });
-  } catch (error) {
-    res.status(500).json({ message: 'Xóa khách hàng không thành công', error: error.message });
-  }
 };
 
 module.exports = {
