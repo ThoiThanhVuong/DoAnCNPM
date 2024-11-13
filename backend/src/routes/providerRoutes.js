6577443a3bdf9ae43da30a847f698d1c6fa19bc2
phu3390 <you@example.com>
2024-11-13 10:27:30 +0700

    sua routes

--- backend/src/routes/providerRoutes.js
+++ backend/src/routes/providerRoutes.js
@@ -4,9 +4,9 @@ const providerController = require('../controllers/providerController');
 
 // Define API routes for Customer
 router.get('/', providerController.getProvider); // Get all Customer
-router.get('/:ma_kh', providerController.getProviderByMaNCC); // Get Customer by ma_kh
+router.get('/:ma_ncc', providerController.getProviderByMaNCC); // Get Customer by ma_kh
 router.post('/', providerController.addProvider); // Create new Customer
-router.put('/:ma_kh', providerController.updateProvider); // Update Customer by ma_kh
-router.delete('/:ma_kh', providerController.deleteProvider); // Delete Customer by ma_kh
+router.put('/:ma_ncc', providerController.updateProvider); // Update Customer by ma_kh
+router.delete('/:ma_ncc', providerController.deleteProvider); // Delete Customer by ma_kh
 
 module.exports = router;
