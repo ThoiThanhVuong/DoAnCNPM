7a0269a53d9d472f69776eb7c97886144ccaa2c9
laithethanh <laithanh@1006@gmail.com>
2024-12-02 21:13:39 +0700

    luu them ma_quyen vao localstorage

--- frontend/src/components/SignIn/SignIn.jsx
+++ frontend/src/components/SignIn/SignIn.jsx
@@ -137,6 +137,8 @@ const SignIn = () => {
       if (response) {
         console.log(Cookies.get("token"));
         localStorage.setItem("ma_nv", response.ma_nv);
+        localStorage.setItem("ten_quyen", response.ma_quyen);
+        // console.log(typeof localStorage.getItem("ten_quyen"))
         alert(`CHÀO MỪNG "${response.ten_nv}" ĐÃ ĐĂNG NHẬP THÀNH CÔNG`);
         window.location.href = "/";
         console.log("thành công", response);
